e602e21da54b7f66f5353c92b1c8051b0523663b
RAY-beep <2836782639@qq.com>
2020-03-11 13:52:55 +0800

    Revert "1"
    This reverts commit 875882af98e93c3765be18475b609098eaa6489f.

--- qyTool/src/cn/createqy/Exception/MemoryOverflow.java
+++
@@ -1,9 +0,0 @@
-package cn.createqy.Exception;
-
-public class MemoryOverflow extends Exception {
-    public MemoryOverflow()
-    {
-        System.out.println("out of memory");
-    }
-
-}
--- qyTool/src/cn/createqy/XML/Jsoup.java
+++
@@ -1,4 +0,0 @@
-package cn.createqy.XML;
-
-public class Jsoup {
-}
