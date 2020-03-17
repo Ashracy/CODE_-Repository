3ebc9b8633ac7132e913200b428d8d658d6def01
RAY-beep <2836782639@qq.com>
2020-03-17 08:01:22 +0800

    1

--- pyTool/音视频/vedio.py
+++ pyTool/音视频/vedio.py
@@ -1,31 +1,20 @@
-import threading
-from playsound import playsound
-def run(n):
-    playsound("E:/source/xxm.wav")
-def vedio_demo():
-    face_cascade = cv.CascadeClassifier("D:\\TOOLS\opencv\\build\\etc\\haarshare\\haarcascade_frontalface_default.xml ")
-    capture=cv.VideoCapture('E:/source/xxm.mp4')
-    fps=capture.get(cv.CAP_PROP_FPS)
-    total=capture.get(cv.CAP_PROP_FRAME_COUNT);
-    i=0;
-    rate=int(1000//fps);
-    print(rate)
-    t=time.time()
-    t1= threading.Thread(target=run,args=('t1',))
-    t1.start()
-    while i<total-2:
+import cv2 as cv
+import  numpy as np
 
-        ret,frame=capture.read()
-        faces=face_cascade.detectMultiScale(frame,1.1,3)
+filepath="E:/source/cxk.mp4"
+vediocapture=cv.VideoCapture(filepath)
+h=vediocapture.get(cv.CAP_PROP_FRAME_HEIGHT)
+w=vediocapture.get(cv.CAP_PROP_FRAME_WIDTH)
+fps=vediocapture.get(cv.CAP_PROP_FPS)
+videowriter=cv.VideoWriter('test.mp4',
+                           cv.VideoWriter_fourcc('m', 'p', '4', 'v'),
+                           fps,
+                           (int(w),int(h)))
+while 1:
+    ret,frame=vediocapture.read();
+    if ret==False:
+        break;
+    videowriter.write(frame);
 
-        for x,y,w,h in faces:
-            image=frame[y:y+h,x:x+w]
-            image=cv.GaussianBlur(image,(0,0),15)
-            frame[ y:y + h,x:x + w]=image
-
-        cv.imshow('video',frame)
-        cv.waitKey(1)
-        i=i+1
-    print(time.time()-t)
-
-
+    cv.imshow("asd",frame);
+    cv.waitKey(1)
